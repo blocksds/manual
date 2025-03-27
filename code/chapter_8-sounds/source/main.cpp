610516810e3a28f3e71a9555de679b10a6258e1a
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-03-27 21:43:16 +0000

    code: Update examples to use simplified bg setup code

--- code/chapter_8-sounds/source/main.cpp
+++ code/chapter_8-sounds/source/main.cpp
@@ -51,74 +51,12 @@ void initVideo() {
 
   /*  Set the video mode on the main screen. */
   videoSetMode(MODE_5_2D |          // Set the graphics mode to Mode 5
-               DISPLAY_BG2_ACTIVE | // Enable BG2 for display
-               DISPLAY_BG3_ACTIVE | // Enable BG3 for display
                DISPLAY_SPR_ACTIVE | // Enable sprites for display
                DISPLAY_SPR_1D       // Enable 1D tiled sprites
   );
 
   /*  Set the video mode on the sub screen. */
-  videoSetModeSub(MODE_5_2D |          // Set the graphics mode to Mode 5
-                  DISPLAY_BG3_ACTIVE); // Enable BG3 for display
-}
-
-void initBackgrounds() {
-  /*  Set up affine background 3 on main as a 16-bit color background. */
-  REG_BG3CNT = BG_BMP16_256x256 |
-               BG_BMP_BASE(0) | // The starting place in memory
-               BG_PRIORITY(3);  // A low priority
-
-  /*  Set the affine transformation matrix for the main screen background 3
-   *  to be the identity matrix.
-   */
-  REG_BG3PA = 1 << 8;
-  REG_BG3PB = 0;
-  REG_BG3PC = 0;
-  REG_BG3PD = 1 << 8;
-
-  /*  Place main screen background 3 at the origin (upper left of the
-   *  screen).
-   */
-  REG_BG3X = 0;
-  REG_BG3Y = 0;
-
-  /*  Set up affine background 2 on main as a 16-bit color background. */
-  REG_BG2CNT = BG_BMP16_128x128 |
-               BG_BMP_BASE(8) | // The starting place in memory
-               BG_PRIORITY(2);  // A higher priority
-
-  /*  Set the affine transformation matrix for the main screen background 3
-   *  to be the identity matrix.
-   */
-  REG_BG2PA = 1 << 8;
-  REG_BG2PB = 0;
-  REG_BG2PC = 0;
-  REG_BG2PD = 1 << 8;
-
-  /*  Place main screen background 2 in an interesting place. */
-  REG_BG2X = -(SCREEN_WIDTH / 2 - 32) << 8;
-  REG_BG2Y = -32 << 8;
-
-  /*  Set up affine background 3 on the sub screen as a 16-bit color
-   *  background.
-   */
-  REG_BG3CNT_SUB = BG_BMP16_256x256 |
-                   BG_BMP_BASE(0) | // The starting place in memory
-                   BG_PRIORITY(3);  // A low priority
-
-  /*  Set the affine transformation matrix for the sub screen background 3 to
-   *  be the identity matrix.
-   */
-  REG_BG3PA_SUB = 1 << 8;
-  REG_BG3PB_SUB = 0;
-  REG_BG3PC_SUB = 0;
-  REG_BG3PD_SUB = 1 << 8;
-
-  /*
-   *  Place main screen background 3 at the origin (upper left of the screen)
-   */
-  REG_BG3X_SUB = 0;
-  REG_BG3Y_SUB = 0;
+  videoSetModeSub(MODE_5_2D);       // Set the graphics mode to Mode 5
 }
 
 void initSprites(OAMTable *oam, SpriteInfo *spriteInfo) {
@@ -283,30 +221,74 @@ void initSprites(OAMTable *oam, SpriteInfo *spriteInfo) {
 }
 
 void displayStarField() {
+  /*  Set up affine background 3 on main screen as a 16-bit color background. */
+  int id = bgInit(3,
+                  BgType_Bmp16,
+                  BgSize_B16_256x256,
+                  0, // Map base: The starting place in memory
+                  0); // Tile base: Not used in 16-bit backgrounds
+
+  /* Use the lowest possible priority */
+  bgSetPriority(id, 3);
+
+  /* Copy graphics data */
   dmaCopyHalfWords(DMA_CHANNEL, starFieldBitmap, /* This variable is generated
                                                   * for us by grit. */
-                   (uint16 *)BG_BMP_RAM(0),      /* Our address for main
+                   bgGetGfxPtr(id),              /* Our address for main
                                                   * background 3 */
                    starFieldBitmapLen);          /* This length (in bytes) is
                                                   * generated from grit. */
 }
 
 void displayPlanet() {
-  dmaCopyHalfWords(DMA_CHANNEL, planetBitmap, /* This variable is generated for
-                                               * us by grit. */
-                   (uint16 *)BG_BMP_RAM(8),   /* Our address for main
-                                               * background 2 */
-                   planetBitmapLen); /* This length (in bytes) is generated
-                                      * from grit. */
+  /*  Set up affine background 2 on main as a 16-bit color background. */
+  int id = bgInit(2,
+                  BgType_Bmp16,
+                  BgSize_B16_128x128,
+                  8,
+                  0);
+
+  /* Set a low priority, but higher than priority 3 */
+  bgSetPriority(id, 2);
+
+  /*  Place main screen background 2 in an interesting place. */
+  bgSetScroll(id,
+              -(SCREEN_WIDTH / 2 - 32),
+              -32);
+
+  /* Copy graphics data */
+  dmaCopyHalfWords(DMA_CHANNEL, planetBitmap,
+                   bgGetGfxPtr(id),
+                   planetBitmapLen);
 }
 
 void displaySplash() {
-  dmaCopyHalfWords(DMA_CHANNEL, splashBitmap, /* This variable is generated for
-                                               * us by grit. */
-                   (uint16 *)BG_BMP_RAM_SUB(0), /* Our address for sub
-                                                 * background 3 */
-                   splashBitmapLen); /* This length (in bytes) is generated
-                                      * from grit. */
+  /*  Set up affine background 3 on the sub screen as a 16-bit color
+   *  background.
+   */
+  int id = bgInitSub(3,
+                     BgType_Bmp16,
+                     BgSize_B16_256x256,
+                     0,
+                     0);
+
+  /* Use the lowest possible priority */
+  bgSetPriority(id, 3);
+
+  /* Copy graphics data */
+  dmaCopyHalfWords(DMA_CHANNEL, splashBitmap,
+                   bgGetGfxPtr(id),
+                   splashBitmapLen);
+}
+
+void initBackgrounds() {
+  /* Display the backgrounds. */
+  displayStarField();
+  displayPlanet();
+  displaySplash();
+
+  /* Refresh background registers */
+  bgUpdate();
 }
 
 void updateInput(touchPosition *touch) {
@@ -403,11 +385,6 @@ int main() {
   OAMTable *oam = new OAMTable();
   initOAM(oam);
   initSprites(oam, spriteInfo);
-
-  /* Display the backgrounds. */
-  displayStarField();
-  displayPlanet();
-  displaySplash();
 
   /*************************************************************************/
 
